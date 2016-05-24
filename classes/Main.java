771e6fac0569d9ea3534dfdc94556b2a0db04abd
100054421 <100054421@student.swin.edu.au>
2016-05-24 20:05:51 +1000

    added-statement-class
    Added the statement class. Changed TruthTable and TruthTableLogic
    classes to use the statement class.

--- classes/Main.java
+++ classes/Main.java
@@ -5,48 +5,49 @@ import java.util.Scanner;
 /*
  * Author	: Kieran Bates
  * Date	: 10/05/2016
- * Description	: 
+ * Description	: The main file for the assignment
  */
 
 public class Main
 {
-	// menu system for 
-	public static void main(String[] args)
-	{
-		int lOption = 0;
-		
-		do
-		{
-			// menu options
-			System.out.println("----------------------------------------");
-			System.out.println("1: Truth Table");
-			System.out.println("2: Exit");
-			System.out.println("----------------------------------------");
-			
-			System.out.print("Enter a menu option: ");
-			
-			// get input from console
-			Scanner lSC = new Scanner(System.in);
-			lOption = lSC.nextInt();
-			
-			// execute each of the menu options if chosen
-			switch(lOption)
-			{
-				case 1:
-					System.out.println("Processing Truth Table");
-					new TruthTableLogic(args[0]);
-					
-					break;
-				case 2:
-					lSC.close();
-				default:
-					lOption = 2;
-					lSC.close();
-					break;
-			} // end switch
-		} // end do
-		while (lOption != 2);
-		
-		System.out.println("Goodbye");
-	} // end main
+    // menu system for the assignment
+    public static void main(String[] args)
+    {
+        int lOption = 0;
+
+        do
+        {
+            // menu options
+            System.out.println("----------------------------------------");
+            System.out.println("1: Truth Table");
+            System.out.println("2: Exit");
+            System.out.println("----------------------------------------");
+
+            System.out.print("Enter a menu option: ");
+
+            // get input from console
+            Scanner lSC = new Scanner(System.in);
+            lOption = lSC.nextInt();
+
+            // execute each of the menu options if chosen
+            switch (lOption)
+            {
+                case 1:
+                    System.out.println("Processing Truth Table");
+                    new TruthTableLogic(args[0]);
+
+                    break;
+                case 2:
+                    lSC.close();
+                default:
+                    lOption = 2;
+                    lSC.close();
+                    
+                    break;
+            } // end switch
+        } // end do
+        while (lOption != 2);
+
+        System.out.println("Goodbye");
+    } // end main
 } // end Main
--- classes/TruthTable.java
+++ classes/TruthTable.java
@@ -8,86 +8,95 @@ import java.util.ArrayList;
  * Description	:
  */
 
-public class TruthTable {
-	private ArrayList<Statement> fStatements;	// the statements
-	private int fColumns;						// the amount of columns in the truth table
-	private int fRows;							// the amount of rows in the truth table
-	private int fStateSize;						// the size of fStatements array
-	
-	public TruthTable(String aInput)
-	{
-		String[] lStatement = aInput.split(";");
-		this.fStatements = new ArrayList<Statement>();
-		
-		for (String lTempString : lStatement)
-		{
-			Statement lTempState = new Statement(lTempString);
-			fStatements.add(lTempState);
-		} // end foreach
-		
-		this.fStateSize = fStatements.size();
-		this.fColumns = calculateColumns();
-		this.fRows = calculateRows();
-	} // end constructor
-	
-	public ArrayList<Statement> getStatement()
-	{
-		return this.fStatements;
-	} // end getStatement
-	
-	public int getColumns()
-	{
-		return this.fColumns;
-	} // end getColumns
-	
-	public int getRows()
-	{
-		return this.fRows;
-	} // end getRows
-	
-	public int getStatmentsSize()
-	{
-		return this.fStateSize;
-	} // end getStatementsSize
-	
-	public int calculateColumns()
-	{
-		return 0;
-	} // end calculateColumns
-	
-	public int calculateRows()
-	{
-		return 0;
-	} // end calculateRows
-	
-	public void generateTable()
-	{
-		String lOutput = "";
-		
-		System.out.println(lOutput);
-	} // end generateTable
-	
-	public void displayVariables()
-	{
-		int i = 0;
-		
-		for (Statement lTempStatement : fStatements)
-		{
-			System.out.println("Statement: " + i);
-			lTempStatement.displayVariables();
-			
-			i++;
-		} // end foreach
-	} // end displayVariables
-	
-	public void displayStatements()
-	{
-		int i = 0;
-		
-		for (Statement lTempStatement : fStatements)
-		{
-			System.out.println("Line " + i + ": " + lTempStatement.getStatement());
-			i++;
-		} // end foreach
-	} // end displayStatements
+public class TruthTable
+{
+    private ArrayList<Statement> fStatements;	// the statements
+    private int fStateSize;			// the size of fStatements array
+
+    public TruthTable(String aInput)
+    {
+        String[] lStatement = aInput.split(";");
+        this.fStatements = new ArrayList<Statement>();
+
+        for (String lTempString : lStatement)
+        {
+            Statement lTempState = new Statement(lTempString);
+            fStatements.add(lTempState);
+        } // end foreach
+
+        this.fStateSize = fStatements.size();
+    } // end constructor
+
+    public ArrayList<Statement> getStatements()
+    {
+        return this.fStatements;
+    } // end getStatement
+
+    public Statement getStatement(int aIndex) throws ArrayIndexOutOfBoundsException
+    {
+        Statement lTempStatement = null;
+        if (aIndex < fStateSize && aIndex > -1)
+        {
+            lTempStatement = fStatements.get(aIndex);
+        } // end if
+        else
+        {
+            throw new ArrayIndexOutOfBoundsException("Error. Index out of range");
+        } // end else
+
+        return lTempStatement;
+    } // end getStatement
+
+    public int getStatmentsSize()
+    {
+        return this.fStateSize;
+    } // end getStatementsSize
+
+    public void drawTable() throws Exception
+    {
+        for (Statement lTempState : fStatements)
+        {
+            try
+            {
+                lTempState.drawTable();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e);
+            }
+        } // end foreach
+    } // end generateTable
+
+    public void displayVariables()
+    {
+        System.out.println("\nVariables:");
+        
+        for (Statement lTempStatement : fStatements)
+        {
+            System.out.println("Statement: " + lTempStatement.getStatement());
+            lTempStatement.displayVariables();
+        } // end foreach
+    } // end displayVariables
+    
+    public void displaySymbols()
+    {
+        System.out.println("\nSymbols:");
+        
+        for (Statement lTempStatement : fStatements)
+        {
+            System.out.println("Statement: " + lTempStatement.getStatement());
+            lTempStatement.displaySymbols();
+        } // end foreach
+    } // end displayVariables
+
+    public void displayStatements()
+    {
+        int i = 1;
+
+        for (Statement lTempStatement : fStatements)
+        {
+            System.out.println("Line " + i + ": " + lTempStatement.getStatement());
+            i++;
+        } // end foreach
+    } // end displayStatements
 } // end TruthTable
